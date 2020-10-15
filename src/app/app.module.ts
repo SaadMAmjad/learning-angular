eb45f2bb88f9f5b73861d2689e6fcafb8f366993
SaadMAmjad <55886212+SaadMAmjad@users.noreply.github.com>
2020-10-14 20:55:25 -0400

    Removed old files
    Removed old content list and generated a new component of the same name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { Asn1PipePipe } from './asn-1-pipe.pipe';
 import { Asn1DirectiveDirective } from './asn-1-directive.directive';
 import { ContentCardComponent } from './content-card/content-card.component';
+import { ContentListComponent } from './content-list/content-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Asn1PipePipe,
     Asn1DirectiveDirective,
-    ContentCardComponent
+    ContentCardComponent,
+    ContentListComponent
   ],
   imports: [
     BrowserModule
--- src/app/content-card/content-card.component.ts
+++ src/app/content-card/content-card.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {ContentList} from '../helper-files/content-list';
 
 @Component({
   selector: 'app-content-card',
@@ -8,30 +7,9 @@ import {ContentList} from '../helper-files/content-list';
 })
 export class ContentCardComponent implements OnInit {
 
-  contentList: ContentList;
   display: string;
 
   constructor() {
-    this.contentList = new ContentList();
-    this.contentList.increaseCount({
-      id: 0,
-      author: 'Saad',
-      title: 'Saad Amjad',
-      body: 'Stuff about me'
-    });
-    this.contentList.increaseCount({
-      id: 1,
-      author: 'Joe Mama',
-      title: 'Lorem Ipsum',
-      body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry'
-    });
-    this.contentList.increaseCount({
-      id: 2,
-      author: 'Bob Bill',
-      title: 'Ipsum Lorem',
-      body: 'Contrary to popular belief, Lorem Ipsum is not simply random text.'
-    });
-    this.display = this.contentList.userOutput(0) + this.contentList.userOutput(1) + this.contentList.userOutput(2);
   }
 
   ngOnInit(): void {
